6c5e2c3e4714cfbab4cd1e96071c85dbdcfd84f0
Ayub_Che <ajubchechiev@mail.ru>
2022-11-07 13:47:09 +0300

    first commit

---
+++ first.js
@@ -0,0 +1,10 @@
+const name = "Ayub";
+const age = 28;
+
+export default function firstDirecory() {
+    return `My name is ${name}! My age ${age} years!`
+}
+
+console.log(firstDirecory())
+
+export { name, age };
---
+++ second.js
@@ -0,0 +1,4 @@
+import { age, name } from "./first.js"
+import firstDirecory from "./first.js"
+
+console.log(age, name, firstDirecory())
